f82b4c70ed4a6bd60d0cbe2600845b17a789e231
834810071 <834810071@qq.com>
2022-03-27 10:22:58 +0800

    完成Gee原型

--- base1/main.go
+++ base1/main.go
@@ -17,5 +17,7 @@ func indexHandle(w http.ResponseWriter, req *http.Request) {
 }
 
 func helloHandle(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintf(w, "URL.Path = %q\n", req.URL.Path)
+	for k, v := range req.Header {
+		fmt.Fprintf(w, "Header[%q] = %q\n", k, v)
+	}
 }
