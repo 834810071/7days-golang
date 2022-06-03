9e4bea96c482286b20900dd1a502ae38611c111c
834810071 <834810071@qq.com>
2022-06-03 17:24:56 +0800

    finish Panic Recover

---
+++ panic-recover/gee/recovery.go
@@ -0,0 +1,40 @@
+package gee
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"runtime"
+	"strings"
+)
+
+// print stack trace for debug
+func trace(message string) string {
+	var pcs [32]uintptr
+	// Callers 用来返回调用栈的程序计数器, 第 0 个 Caller 是 Callers 本身，
+	// 第 1 个是上一层 trace，第 2 个是再上一层的 defer func。
+	// 因此，为了日志简洁一点，我们跳过了前 3 个 Caller。
+	n := runtime.Callers(3, pcs[:]) // skip first 3 caller
+
+	var str strings.Builder
+	str.WriteString(message + "\nTraceback:")
+	for _, pc := range pcs[:n] {
+		fn := runtime.FuncForPC(pc)
+		file, line := fn.FileLine(pc)
+		str.WriteString(fmt.Sprintf("\n\t%s:%d", file, line))
+	}
+	return str.String()
+}
+
+func Recovery() HandlerFunc {
+	return func(c *Context) {
+		defer func() {
+			if err := recover(); err != nil {
+				message := fmt.Sprintf("%s", err)
+				log.Printf("%s\n\n", trace(message))
+				c.Fail(http.StatusInternalServerError, "Internal Server Error")
+			}
+		}()
+		c.Next()
+	}
+}
